04601f5c572b08d8de5b567c80b5ada466304b7e
JphudsonUA <91239313+JphudsonUA@users.noreply.github.com>
2022-02-23 19:39:31 -0600

    Add files via upload

--- LispInterpreter.py
+++ LispInterpreter.py
@@ -1,3 +1,4 @@
+from ast import parse
 from curses.ascii import SP, isalnum, isalpha
 import re
 
@@ -35,7 +36,38 @@ def Expression(line, expr):
     if expr == '>':
         return
     if expr == 'cons':
-        return
+        consList = []
+        arg1 = []
+        arg2 = []
+        x = ''
+        y = ''
+        while x != '(':
+            x = line[0]
+            line.pop(0)
+            print("X is ", x)
+            print("line 0 is ", line[0])
+            if(x == ')'):
+                break
+            if(x == '('):
+                x = line[0]
+                line.pop(0)
+                print(line[0])
+            arg1.append(Expression(line, x))
+            x = line[0]
+            
+        while y != '(':
+            y = line[0]
+            line.pop(0)
+            if(y == ')'):
+                break
+            if(y == '('):
+                y = line[0]
+                line.pop(0)
+            arg2.append(Expression(line, y))
+            y = line[0]
+        consList.append(arg1)
+        consList.append(arg2)
+        return consList
     if expr == 'car':
         return
     if expr == 'cdr':
@@ -57,6 +89,9 @@ def Expression(line, expr):
         return
     if expr == 'call':
         return
+    if expr == ')':
+        return
+    return expr
 
 def SplitTokens(line):
     pattern = re.compile('''((?:[^ "']|"[^"]*"|'[^']*')+)''')
@@ -68,6 +103,7 @@ def SplitTokens(line):
     return tokenList 
 
 def Parse(line):
+    mylist = []
     for expr in line:
         line.pop(0)
         if expr == '(':
@@ -80,10 +116,12 @@ def Parse(line):
         if expr == ']':
             return
         return expr
-    return
+    return mylist
 
 a = "(print \"hello everyone\")"
 b = "(print (- (/ (* (+ 7 4) 3)) 11) 2)"
+c = "(print (cons 3 (4 5)))"
 print(SplitTokens(b))
 Parse(SplitTokens(a))
 Parse(SplitTokens(b))
+Parse(SplitTokens(c))
